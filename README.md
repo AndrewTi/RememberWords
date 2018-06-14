3062d3aee8ba67fa54ace120d8159ce224fd5f0f
Andrey Tyndyk <andreytyndyk@gmail.com>
2018-06-14 12:55:38 +0300

    a bit updated readme file also added new logic and functions

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ### How to install
 tutorial how to [install](https://developer.chrome.com/extensions/getstarted) extestion to chrome <br>
-also need to have need
+also need to have
--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 (() => {
     const id = id => document.getElementById(id);
 
+    const curr = {
+        word: null,
+        data: null
+    };
+    const baseUrl = 'http://localhost:5000/rm-words/us-central1';
+
     const sendText = id('send-text');
     const inputText = id('translate-it');
     const outputText = id('output-text');
+    const textBlock = id('text-block');
+    const addWord = id('add-word');
 
     let header = new Headers({
         'Access-Control-Allow-Origin':'*',
         'Content-Type': 'application/json'
     });
 
+    addWord.addEventListener('click', () => {
+        fetch(baseUrl + '/addWrod', {
+            method: "POST",
+            header,
+            body: JSON.stringify({word: curr.word, data: curr.data})
+        }).then(resp => {
+            return resp.json();
+        }).then(data => {
+            console.log(data);
+        }).catch(err => {
+            alert(err.message);
+            console.log(err);
+        })
+    })
+
     sendText.addEventListener('click', () => {
         const text = inputText.value;
 
-        fetch(`https://translation.googleapis.com/language/translate/v2?key=AIzaSyDpiyel9X3GnOm2TbgLcOj_N8O_Knus8qQ&q=${text}&target=uk&format=text`, {
+        fetch(baseUrl + '/translate', {
             method: "POST",
-            header
+            header,
+            body: JSON.stringify({targLang: 'uk', sourceLang: 'auto', text: text})
         }).then(text => {
             text.json().then(data => {
-                outputText.classList.remove('hide');
-                outputText.innerText = data.data.translations[0].translatedText;
+                curr.word = data.sentences[0].orig;
+                curr.data = JSON.stringify(data);
+
+                console.log(data.sentences[0].trans)
+                textBlock.classList.remove('hide');
+                outputText.innerText = data.sentences[0].trans;
                 console.log(data, 'gav');
             }).catch(err => {
                 outputText.innerText = 'cannot to translate'
