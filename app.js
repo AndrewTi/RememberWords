3f4e2c542673341afcab0b00e11cc32b48742c09
yarynkamazuryk <yarynkafox@gmail.com>
2018-09-14 17:19:38 +0300

    Add translate functionality

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 (() => {
     const id = id => document.getElementById(id);
-
     const curr = {
         word: null,
         data: null
     };
-    const baseUrl = 'http://localhost:5000/rm-words/us-central1';
+    const baseUrl = 'https://us-central1-rm-words.cloudfunctions.net';
 
     const sendText = id('send-text');
     const inputText = id('translate-it');
     const outputText = id('output-text');
-    const textBlock = id('text-block');
+    const description = id('description');
     const addWord = id('add-word');
+    const download = id('download');
 
     let header = new Headers({
-        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json'
+    });
+
+    download.addEventListener('click', () => {
+        fetch(baseUrl + '/toPDF', {
+            method: "GET",
+            header,
+            mode: 'cors',
+            cache: 'default'
+        }).then(data => {
+            window.open(`${data.url}`);
+        }).catch(err => {
+            alert(err.message);
+            console.log(err);
+        })
     });
 
     addWord.addEventListener('click', () => {
         fetch(baseUrl + '/addWord', {
             method: "POST",
             header,
-            body: JSON.stringify({word: curr.word, data: curr.data})
+            body: JSON.stringify({ word: curr.word, data: curr.data })
         }).then(resp => {
             return resp.json();
         }).then(data => {
@@ -39,22 +53,35 @@
         fetch(baseUrl + '/translate', {
             method: "POST",
             header,
-            body: JSON.stringify({targLang: 'uk', sourceLang: 'auto', text: text})
+            body: JSON.stringify({ targLang: 'uk', sourceLang: 'auto', text: text })
         }).then(text => {
             text.json().then(data => {
                 curr.word = data.sentences[0].orig;
                 curr.data = JSON.stringify(data);
-
                 console.log(data.sentences[0].trans)
-                textBlock.classList.remove('hide');
                 outputText.innerText = data.sentences[0].trans;
-                console.log(data, 'gav');
+
             }).catch(err => {
                 outputText.innerText = 'cannot to translate'
             })
-            
+
         }).catch(err => {
             outputText.innerText = 'cannot to translate'
         })
+
+        // testing the output of all words in json-format
+        // download.addEventListener('click', () => {
+        //     fetch (baseUrl + '/getAllWords', {
+        //         method: "GET",
+        //         header,
+        //         mode: 'cors',
+        //         cache: 'default'
+        //     }).then(data => {
+        //         window.open(`${data.url}`);
+        //     }).catch(err => {
+        //         alert(err.message);
+        //         console.log(err);
+        //     })
+        // });
     })
 })()
