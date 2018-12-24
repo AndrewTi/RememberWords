ebeab66f20fb46b0842345a49e89c5cf11cc2922
Svitlana Melashenko <svetapogranichna@gmail.com>
2018-12-24 22:46:41 +0200

    add markup for registration and login

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
+// singleton pattern
 (() => {
+
+    // facade pattern
     const id = id => document.getElementById(id);
+
     const curr = {
         word: null,
         data: null
     };
     const baseUrl = 'https://us-central1-rm-words.cloudfunctions.net';
 
-    const sendText = id('send-text');
-    const inputText = id('translate-it');
-    const outputText = id('output-text');
-    const description = id('description');
-    const addWord = id('add-word');
-    const download = id('download');
+    const sendText      = id('send-text');
+    const inputText     = id('translate-it');
+    const outputText    = id('output-text');
+    const description   = id('description');
+    const addWord       = id('add-word');
+    const download      = id('download');
+
+    const enterForm         = document.querySelector('.enter-form');
+    const login             = document.querySelector(".login");
+    const registration      = document.querySelector('.registration');
+    const registrationForm  = document.querySelector('.registration-form');
+    const loginForm         = document.querySelector('.login-form');
+    const mainContent       = document.querySelector('.main');
+    const homeButton        = document.querySelector('.homebtn');
+    const signup            = document.querySelector('.signup');
+    const signin            = document.querySelector('.signin');
 
     let header = new Headers({
         'Access-Control-Allow-Origin': '*',
@@ -68,20 +82,62 @@
         }).catch(err => {
             outputText.innerText = 'cannot to translate'
         })
+    })
 
-        // testing the output of all words in json-format
-        // download.addEventListener('click', () => {
-        //     fetch (baseUrl + '/getAllWords', {
-        //         method: "GET",
-        //         header,
-        //         mode: 'cors',
-        //         cache: 'default'
-        //     }).then(data => {
-        //         window.open(`${data.url}`);
-        //     }).catch(err => {
-        //         alert(err.message);
-        //         console.log(err);
-        //     })
-        // });
+    login.addEventListener('click', () => {
+        // show
+        loginForm.classList.remove('hide');
+
+        // hide
+        enterForm.classList.add('hide');
+        login.classList.add('hide');
+        registration.classList.add('hide');
+        mainContent.classList.add('hide');
+        registrationForm.classList.add('hide');
+    })
+
+    registration.addEventListener('click', () => {
+        // show
+        registrationForm.classList.remove('hide');
+
+        // hide
+        enterForm.classList.add('hide');
+        login.classList.add('hide');
+        registration.classList.add('hide');
+        mainContent.classList.add('hide')
+        loginForm.classList.add('hide');
+    })
+
+    homeButton.addEventListener('click', ()=> {
+        //show
+        mainContent.classList.remove('hide');
+        enterForm.classList.remove('hide');
+        login.classList.remove('hide');
+        registration.classList.remove('hide');
+        //hide
+        loginForm.classList.add('hide');
+        registrationForm.classList.add('hide');
+    })
+
+    signup.addEventListener('click', ()=> {
+        // show
+        registrationForm.classList.remove('hide');
+        // hide
+        enterForm.classList.add('hide');
+        login.classList.add('hide');
+        registration.classList.add('hide');
+        mainContent.classList.add('hide');
+        loginForm.classList.add('hide');
+    })
+
+    signin.addEventListener('click', ()=> {
+        // show
+        loginForm.classList.remove('hide');
+        // hide
+        registrationForm.classList.add('hide');
+        enterForm.classList.add('hide');
+        login.classList.add('hide');
+        registration.classList.add('hide');
+        mainContent.classList.add('hide');
     })
 })()
